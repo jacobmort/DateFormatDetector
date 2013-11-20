29f55f3f60f244fe495a2dbd19cccd0566f7b81e
Jacob Mortensen <jacob-old@montipayincsmbp.(none)>
2013-11-20 01:27:26 -0500

    support other delimiters

--- date_detector.rb
+++ date_detector.rb
@@ -2,44 +2,61 @@ require 'set'
 require 'date'
 
 class DateDetector
-  @@dDELIMITERS = [ /\s+/, /\//, /\-/, /\./ ]
+  @@DELIMITERS = [" ","/",'-',"."]
+  @@DEFAULT_DELIM = "/"
   @@REGEX_DATE_MAP = {
-    /^([0]?[1-9]|1[0-2])(\/)([0-9]|[012]\d|3[0-1])(\/)\d{2}(\s|\z)/ => "%m/%d/%y",
-    /^([0]?[1-9]|1[0-2])(\/)([0-9]|[012]\d|3[0-1])(\/)\d{4}(\s|\z)/ => "%m/%d/%Y",
-    /^([0-9]|[012]\d|3[0-1])(\/)([0]?[1-9]|1[0-2])(\/)\d{2}(\s|\z)/ => "%d/%m/%y",
-    /^([0-9]|[012]\d|3[0-1])(\/)([0]?[1-9]|1[0-2])(\/)\d{4}(\s|\z)/ => "%d/%m/%Y",
+    /^([0]?[1-9]|1[0-2])([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])\d{2}(\s|\z)/ => "%m/%d/%y",
+    /^([0]?[1-9]|1[0-2])([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])\d{4}(\s|\z)/ => "%m/%d/%Y",
+    /^([0-9]|[012]\d|3[0-1])([\/\s\.\-])([0]?[1-9]|1[0-2])([\/\s\.\-])\d{2}(\s|\z)/ => "%d/%m/%y",
+    /^([0-9]|[012]\d|3[0-1])([\/\s\.\-])([0]?[1-9]|1[0-2])([\/\s\.\-])\d{4}(\s|\z)/ => "%d/%m/%Y",
 
-    /^\d{2}(\/)([0-9]|[012]\d|3[0-1])(\/)([0]?[1-9]|1[0-2])(\s|\z)/ => "%y/%d/%m",
-    /^\d{4}(\/)([0-9]|[012]\d|3[0-1])(\/)([0]?[1-9]|1[0-2])(\s|\z)/ => "%Y/%d/%m",
-    /^\d{2}(\/)([0]?[1-9]|1[0-2])(\/)([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%y/%m/%d",
-    /^\d{4}(\/)([0]?[1-9]|1[0-2])(\/)([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%Y/%m/%d",
+    /^\d{2}([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])([0]?[1-9]|1[0-2])(\s|\z)/ => "%y/%d/%m",
+    /^\d{4}([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])([0]?[1-9]|1[0-2])(\s|\z)/ => "%Y/%d/%m",
+    /^\d{2}([\/\s\.\-])([0]?[1-9]|1[0-2])([\/\s\.\-])([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%y/%m/%d",
+    /^\d{4}([\/\s\.\-])([0]?[1-9]|1[0-2])([\/\s\.\-])([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%Y/%m/%d",
 
     #Month spelled out ex. Sep
-    /^([0-9]|[012]\d|3[0-1])(\/)[a-zA-Z]{3}(\/)\d{2}(\s|\z)/ => "%d/%b/%y",
-    /^([0-9]|[012]\d|3[0-1])(\/)[a-zA-Z]{3}(\/)\d{4}(\s|\z)/ => "%d/%b/%Y",
-    /^[a-zA-Z]{3}(\/)([0-9]|[012]\d|3[0-1])(\/)\d{2}(\s|\z)/ => "%b/%d/%y",
-    /^[a-zA-Z]{3}(\/)([0-9]|[012]\d|3[0-1])(\/)\d{4}(\s|\z)/ => "%b/%d/%Y",
+    /^([0-9]|[012]\d|3[0-1])([\/\s\.\-])[a-zA-Z]{3}([\/\s\.\-])\d{2}(\s|\z)/ => "%d/%b/%y",
+    /^([0-9]|[012]\d|3[0-1])([\/\s\.\-])[a-zA-Z]{3}([\/\s\.\-])\d{4}(\s|\z)/ => "%d/%b/%Y",
+    /^[a-zA-Z]{3}([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])\d{2}(\s|\z)/ => "%b/%d/%y",
+    /^[a-zA-Z]{3}([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])\d{4}(\s|\z)/ => "%b/%d/%Y",
 
 
-    /^\d{2}(\/)([0-9]|[012]\d|3[0-1])(\/)[a-zA-Z]{3}(\s|\z)/ => "%y/%d/%b",
-    /^\d{4}(\/)([0-9]|[012]\d|3[0-1])(\/)[a-zA-Z]{3}(\s|\z)/ => "%Y/%d/%b",
-    /^\d{2}(\/)[a-zA-Z]{3}(\/)([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%y/%b/%d",
-    /^\d{4}(\/)[a-zA-Z]{3}(\/)([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%Y/%b/%d",
+    /^\d{2}([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])[a-zA-Z]{3}(\s|\z)/ => "%y/%d/%b",
+    /^\d{4}([\/\s\.\-])([0-9]|[012]\d|3[0-1])([\/\s\.\-])[a-zA-Z]{3}(\s|\z)/ => "%Y/%d/%b",
+    /^\d{2}([\/\s\.\-])[a-zA-Z]{3}([\/\s\.\-])([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%y/%b/%d",
+    /^\d{4}([\/\s\.\-])[a-zA-Z]{3}([\/\s\.\-])([0-9]|[012]\d|3[0-1])(\s|\z)/ => "%Y/%b/%d",
 
   }
+
+  def determine_delimiter_regex(str)
+    @@DELIMITERS.each do |possible_delimter|
+      if (str.include? possible_delimter)
+        return possible_delimter
+      end
+    end
+    nil
+  end
 
   def does_match(regex, str)
     (str.match regex) != nil
   end
 
+  def replace_delimiter(format, delim)
+    format.gsub(@@DEFAULT_DELIM, delim)
+  end
+
   def get_date_format(str)
     @possible_formats = Set.new()
-    @@REGEX_DATE_MAP.each do |regex,format|
-      @possible_formats.add(format);
-    end
-    @@REGEX_DATE_MAP.each do |regex,format|
-      unless does_match(regex, str)
-        @possible_formats.delete(format)
+    delimit = determine_delimiter_regex(str)
+    unless delimit.nil?
+      @@REGEX_DATE_MAP.each do |regex,format|
+        @possible_formats.add(replace_delimiter(format,delimit))
+      end
+      @@REGEX_DATE_MAP.each do |regex,format|
+        unless does_match(regex, str)
+          @possible_formats.delete(replace_delimiter(format,delimit))
+        end
       end
     end
     @possible_formats.to_a
@@ -49,24 +66,13 @@ class DateDetector
     format = get_date_format(str)
     if format.length == 0
       #puts "could not determine format of:"+str
-      -1
+      nil
     elsif format.length > 1
       #puts "multiple formats matched:"<<str
       #puts "formats:"<<formats.to_s
-      -1
+      nil
     else
       Date.strptime(str, format[0])
     end
+  end
 end
-
-end
-#1/3/12
-#16-Sep-12
-#25.08.2011
-#Feb 12 2010  6:02AM
-#2011-05-14 22:16:00 -0400
-#13/08/2009
-#3/3/12
-#20121111
-dd = DateDetector.new()
-date = dd.get_ruby_date("12/13/Jun")
--- date_detector_test.rb
+++ date_detector_test.rb
@@ -9,29 +9,40 @@ class DateDectectorTests < Test::Unit::TestCase
   def teardown
     # Do nothing
   end
+  #1/3/12
+  #16-Sep-12
+  #25.08.2011
+  #13/08/2009
+
+  #Feb 12 2010  6:02AM
+  #2011-05-14 22:16:00 -0400
+  #3/3/12
+  #20121111
 
   def test_broken_str
-    assert_equal(-1 , @dd.get_ruby_date(""))
-    assert_equal(-1 , @dd.get_ruby_date("1"))
-    assert_equal(-1 , @dd.get_ruby_date("1/1"))
-    assert_equal(-1 , @dd.get_ruby_date("1/1/1"))
-    assert_equal(-1 , @dd.get_ruby_date("a"))
-    assert_equal(-1 , @dd.get_ruby_date("1/33/2013"))
-    assert_equal(-1 , @dd.get_ruby_date("33/1/2013"))
-    assert_equal(-1 , @dd.get_ruby_date("13/24/2013"))
-    assert_equal(-1 , @dd.get_ruby_date("24/13/2013"))
+    assert_equal(nil , @dd.get_ruby_date(""))
+    assert_equal(nil , @dd.get_ruby_date("1"))
+    assert_equal(nil , @dd.get_ruby_date("1/1"))
+    assert_equal(nil , @dd.get_ruby_date("1/1/1"))
+    assert_equal(nil , @dd.get_ruby_date("a"))
+    assert_equal(nil , @dd.get_ruby_date("1/33/2013"))
+    assert_equal(nil , @dd.get_ruby_date("33/1/2013"))
+    assert_equal(nil , @dd.get_ruby_date("13/24/2013"))
+    assert_equal(nil , @dd.get_ruby_date("24/13/2013"))
   end
 
   def test_ambigious
-    assert_equal(-1 , @dd.get_ruby_date("1/3/12"))
-    assert_equal(-1 , @dd.get_ruby_date("1/12/12"))
-    assert_equal(-1 , @dd.get_ruby_date("01/13/12"))
-    assert_equal(-1 , @dd.get_ruby_date("13/1/12"))
+    assert_equal(nil , @dd.get_ruby_date("1/3/12"))
+    assert_equal(nil , @dd.get_ruby_date("1/12/12"))
+    assert_equal(nil , @dd.get_ruby_date("01/13/12"))
+    assert_equal(nil , @dd.get_ruby_date("13/1/12"))
   end
 
   def test_m_d_y
     dateExamples = {
       "1/13/12" => Date.new(2012,1,13),
+      "1.13.12" => Date.new(2012,1,13),
+      "1 13 12" => Date.new(2012,1,13),
       "01/13/2012" => Date.new(2012,1,13),
       "12/21/2013" => Date.new(2013,12,21),
       "Jan/13/12" => Date.new(2012,1,13),
@@ -48,6 +59,7 @@ class DateDectectorTests < Test::Unit::TestCase
   def test_d_m_y
     dateExamples = {
         "13/01/2012" => Date.new(2012,1,13),
+        "13-01-2012" => Date.new(2012,1,13),
         "21/12/2013" => Date.new(2013,12,21),
         "13/Apr/2012" => Date.new(2012,4,13),
         "21/May/2013" => Date.new(2013,5,21),
@@ -65,6 +77,9 @@ class DateDectectorTests < Test::Unit::TestCase
         "2012/13/01" => Date.new(2012,1,13),
         "2013/21/12" => Date.new(2013,12,21),
         "12/13/Jun" => Date.new(2012,6,13),
+        "12 13 Jun" => Date.new(2012,6,13),
+        "12-13-Jun" => Date.new(2012,6,13),
+        "12.13.Jun" => Date.new(2012,6,13),
         "2012/13/Jul" => Date.new(2012,7,13),
         "2013/21/Aug" => Date.new(2013,8,21),
     }
